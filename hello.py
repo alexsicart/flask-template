e159e9280865c284d438c08467dcec335f784f7e
Alex Sicart Ramos <alex@sharge.io>
2017-10-22 02:55:46 +0200

    improves

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/')
+def index():
+    return 'Index Page'
